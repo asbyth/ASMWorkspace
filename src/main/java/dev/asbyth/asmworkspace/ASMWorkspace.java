f02622769d995649b23ec5fe8b806d07f971f83d
asbyth <cyronize@gmail.com>
2019-08-12 05:35:18 -0400

    init

---
+++ src/main/java/dev/asbyth/asmworkspace/ASMWorkspace.java
@@ -0,0 +1,7 @@
+package dev.asbyth.asmworkspace;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "asm_workspace", name = "ASM Workspace", version = "1.0")
+public class ASMWorkspace {
+}
